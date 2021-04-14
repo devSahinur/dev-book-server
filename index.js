b1b50d6c025f9b5193bf77b06423f14ef87d0319
Sahinur Islam <infosahinur@gmail.com>
2021-04-14 21:19:44 +0600

    new

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zupo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -10,7 +11,7 @@ const app = express()
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -47,6 +48,19 @@ client.connect(err => {
         ordersCollection.find({})
         .toArray((err, document) => {
             res.send(document)
+        })
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        ordersCollection.deleteOne({_id: ObjectId(req.params.id)})
+        .then( result => {
+            console.log(result);
+        })
+    })
+    app.delete('/post/:id', (req, res) => {
+        booksCollection.deleteOne({_id: ObjectId(req.params.id)})
+        .then( result => {
+            console.log(result);
         })
     })
 
